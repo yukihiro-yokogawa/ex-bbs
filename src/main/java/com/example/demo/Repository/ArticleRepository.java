6ec7e7a3d3878e44d94a990f2a2b887abddd285c
yukihiro-yokogawa <yukihiro.yokogawa@rakus-partners.co.jp>
2019-08-08 14:35:33 +0900

    リポジトリクラスに挿入メソッドを作成

--- src/main/java/com/example/demo/Repository/ArticleRepository.java
+++ src/main/java/com/example/demo/Repository/ArticleRepository.java
@@ -1,12 +1,17 @@
-package com.example.demo.Domein;
+package com.example.demo.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
+
+import com.example.demo.Domein.Article;
+import com.example.demo.Domein.Comment;
 
 @Repository
 public class ArticleRepository {
@@ -15,23 +20,23 @@ public class ArticleRepository {
 	private NamedParameterJdbcTemplate template;
 
 	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
-		
+
 		Article article = new Article();
 		article.setId(rs.getInt("art.id"));
 		article.setName(rs.getString("art.name"));
 		article.setContent(rs.getString("art.content"));
-		
+
 		Comment comment = new Comment();
 		comment.setId(rs.getInt("cmt.id"));
 		comment.setName(rs.getString("cmt.name"));
 		comment.setContent(rs.getString("cmt.content"));
 		comment.setArticleld(rs.getInt("cmt.article_id"));
-		
+
 		List<Comment> commentList = new ArrayList<>();
 		commentList.add(comment);
-		
+
 		article.setCommentList(commentList);
-		
+
 		return article;
 	};
 
@@ -45,8 +50,32 @@ public class ArticleRepository {
 		String sql = "SELECT art.id,art.name,art.content,cmt.id,cmt.name,cmt.content,cmt.article_id FROM articles art JOIN comments cmt ON art.id = cmt.article_id";
 
 		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
-		
+
 		return articleList;
+	}
+
+	/**
+	 * 記事、又はコメントを挿入するメソッドです。
+	 * 
+	 * @param article
+	 * @param comment
+	 */
+	public void insert(Article article, Comment comment) {
+
+		SqlParameterSource paramArt = new BeanPropertySqlParameterSource(article);
+		SqlParameterSource paramCmt = new BeanPropertySqlParameterSource(comment);
+
+		if (article.getId() == null) {
+			String sqlArt = "INSERT INTO articles (name,content) VALUES (:name,:content)";
+
+			template.update(sqlArt, paramArt);
+
+		} else if (comment.getId() == null) {
+			String sqlCmt = "INSERT INTO comments (name,content,article_id) VALUES (:name,:content,article_id)";
+			
+			template.update(sqlCmt, paramCmt);
+		}
+
 	}
 
 }
