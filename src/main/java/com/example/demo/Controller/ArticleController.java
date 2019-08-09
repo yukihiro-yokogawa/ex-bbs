658c9836fca93a9ead635f1861aea9c8ffffc1cc
yukihiro-yokogawa <yukihiro.yokogawa@rakus-partners.co.jp>
2019-08-09 09:49:40 +0900

    //記事とコメントを検索するメソッドの改修

--- src/main/java/com/example/demo/Controller/ArticleController.java
+++ src/main/java/com/example/demo/Controller/ArticleController.java
@@ -1,5 +1,36 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.Repository.ArticleRepository;
+
+/**
+ * 機能とViewを操作するコントローラクラスです.
+ * 
+ * @author yukihiro.yokogawa
+ *
+ */
+@Controller
+@RequestMapping("/bbs")
 public class ArticleController {
+
+	@Autowired
+	private ArticleRepository articleRepository;
+	
+	
+	/**
+	 * 記事,コメントを検索する機能を呼び出し、bbsページに遷移させるメソッドです.
+	 * 
+	 * @param model リクエストスコープ
+	 * @return 掲示板
+	 */
+	@RequestMapping("")
+	public String index(Model model) {
+		model.addAttribute("bbsList",articleRepository.findAll());
+		return "bbs";
+	}
 
 }
--- src/main/java/com/example/demo/Repository/ArticleRepository.java
+++ src/main/java/com/example/demo/Repository/ArticleRepository.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -13,32 +13,81 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.Domein.Article;
 import com.example.demo.Domein.Comment;
 
+/**
+ * BBSの記事、コメント欄を実際に操作するメソッドです.
+ * 
+ * @author yukihiro.yokogawa
+ *
+ */
 @Repository
 public class ArticleRepository {
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 
-	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
+	/**	現在検索中の記事IDと前回の記事IDを比較し、場合によってコメントのみ取り出すメソッドです. */
+	private static final ResultSetExtractor<List<Article>> ARTICLE_EXTRACTOR = (rs) -> {
 
-		Article article = new Article();
-		article.setId(rs.getInt("art.id"));
-		article.setName(rs.getString("art.name"));
-		article.setContent(rs.getString("art.content"));
+		List<Article> articleList = new ArrayList<>();
+		List<Comment> commentList = null;
+		
+		//記事ID退避用の変数
+		int articleIdBefore = -1;
+		
+		//次の行を検索（初めて検索するときは1行目
+		while (rs.next()) {
+			
+			//現在検索している記事ID
+			int articleIdNow = rs.getInt("art_id");
+			
+			//記事IDを比較し、一致しなければArrayList型のcommentオブジェクトを含んだarticleオブジェクトを新規で作成
+			if (articleIdBefore != articleIdNow) {
 
-		Comment comment = new Comment();
-		comment.setId(rs.getInt("cmt.id"));
-		comment.setName(rs.getString("cmt.name"));
-		comment.setContent(rs.getString("cmt.content"));
-		comment.setArticleld(rs.getInt("cmt.article_id"));
+				Article article = new Article();
+				article.setId(rs.getInt("art_id"));
+				article.setName(rs.getString("art_name"));
+				article.setContent(rs.getString("art_content"));
 
-		List<Comment> commentList = new ArrayList<>();
-		commentList.add(comment);
+				commentList = new ArrayList<>();
+				article.setCommentList(commentList);
+				
+				//今回検索した記事IDを変数に退避
+				articleIdBefore = articleIdNow;
+				articleList.add(article);
+			}
+			
+			//コメント情報を含んだcommentオブジェクトを、前回作ったarticleオブジェクト内のcommentオブジェクトに入れる
+			Comment comment = new Comment();
+			comment.setId(rs.getInt("cmt_id"));
+			comment.setName(rs.getString("cmt_name"));
+			comment.setContent(rs.getString("cmt_content"));
+			comment.setArticleld(rs.getInt("cmt_article_id"));
+			commentList.add(comment);
 
-		article.setCommentList(commentList);
-
-		return article;
+		}
+		return articleList;
 	};
+
+//	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
+//
+//		Article article = new Article();
+//		article.setId(rs.getInt("art_id"));
+//		article.setName(rs.getString("art_name"));
+//		article.setContent(rs.getString("art_content"));
+//
+//		Comment comment = new Comment();
+//		comment.setId(rs.getInt("cmt_id"));
+//		comment.setName(rs.getString("cmt_name"));
+//		comment.setContent(rs.getString("cmt_content"));
+//		comment.setArticleld(rs.getInt("cmt_article_id"));
+//
+//		List<Comment> commentList = new ArrayList<>();
+//		commentList.add(comment);
+//
+//		article.setCommentList(commentList);
+//
+//		return article;
+//	};
 
 	/**
 	 * 記事とコメントを全検索するメソッドです.
@@ -47,9 +96,9 @@ public class ArticleRepository {
 	 */
 	public List<Article> findAll() {
 
-		String sql = "SELECT art.id,art.name,art.content,cmt.id,cmt.name,cmt.content,cmt.article_id FROM articles art JOIN comments cmt ON art.id = cmt.article_id";
+		String sql = "SELECT art.id AS art_id,art.name AS art_name,art.content AS art_content,cmt.id AS cmt_id,cmt.name AS cmt_name,cmt.content AS cmt_content, cmt.article_id AS cmt_article_id FROM articles art JOIN comments cmt ON art.id = cmt.article_id";
 
-		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
+		List<Article> articleList = template.query(sql, ARTICLE_EXTRACTOR);
 
 		return articleList;
 	}
@@ -72,7 +121,7 @@ public class ArticleRepository {
 
 		} else if (comment.getId() == null) {
 			String sqlCmt = "INSERT INTO comments (name,content,article_id) VALUES (:name,:content,article_id)";
-			
+
 			template.update(sqlCmt, paramCmt);
 		}
 
